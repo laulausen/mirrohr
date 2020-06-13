5b588c9f44c8196a67eedad00037fc704162c34c
Your Name <you@example.com>
2020-06-13 21:15:34 +0100

    small changes in make-a-mirrohr script

--- files/home/pi/startBrowser.sh
+++ files/home/pi/startBrowser.sh
@@ -4,7 +4,7 @@ xset -dpms # disable DPMS (Energy Star) features.
 xset s off # disable screen saver
 xset s noblank # don't blank the video device
 # unclutter -idle 0.01 -root &
-xrandr --output HDMI-1 --rotate left
+xrandr --output HDMI-1 --rotate right
 matchbox-window-manager -use_titlebar no -use_cursor no &
 sudo -u pi -H chromium-browser \
 	--display=:0.0 \
@@ -12,5 +12,6 @@ sudo -u pi -H chromium-browser \
 	--disable-translate \
 	--app-auto-launched \
 	--incognito \
-	--kiosk http://localhost/$1
+	--app=http://localhost/$1
+#	--kiosk http://localhost/$1
 
